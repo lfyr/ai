bf5f876d5fa1061967729dd57f8d8b45494ebc61
unknown <wangchaoyi@epaynfc.com>
2025-02-18 15:45:51 +0800

    [无任务]代码优化

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const { storeChunks, searchChunks, generateWithOllama } = require("./service/documents")
-const { chat } = require("./utils/siliconflow")
+const { chat, rerank } = require("./utils/siliconflow")
 async function main() {
 
-    // 导入知识库
-    // await storeChunks("./knowledge/rqbs.pdf")
+    // // 一、导入知识库
+    // // await storeChunks("./knowledge/rqbs.pdf")
 
-    /*******************/
-    // 检索知识库
-    const question = "维纳股份"
-    // 查询database 中匹配到的数据
-    const queryDbData = await searchChunks(question)
-    const context = queryDbData.map(item => item.content).join('\n');
+    // /*******************/
+    // // 检索知识库
+    // const question = "维纳股份"
+    // // 查询database 中匹配到的数据
+    // const queryDbData = await searchChunks(question)
+    // const context = queryDbData.map(item => item.content).join('\n');
 
-    // 调用模型生成结果
-    const response = await generateWithOllama(question, context)
-    console.info(`response:${response}`);
+    // // 调用模型生成结果
+    // const response = await generateWithOllama(question, context)
+    // console.info(`response:${response}`);
 
-    // // 硅基流动
-    // const message = [
-    //     { role: "user", content: "Who won the world series in 2020?" },
-    //     { role: "assistant", content: "The Los Angeles Dodgers won the World Series in 2020." },
-    //     { role: "user", content: "Where was it played?" }
-    // ]
-    // const model = "deepseek-ai/DeepSeek-R1"
-    // const content = await chat(message, model)
-    // console.log(content)
+    // //二、 硅基流动
+    // const query = "人工智能的应用场景"; // 搜索关键词
+    // const documents = ["人工智能在医疗领域的应用", "人工智能在教育领域的应用", "人工智能在金融领域的应用", "人工智能在农业领域的应用"]; // 文档列表
+    // const model = "netease-youdao/bce-reranker-base_v1"; // 模型
+    // const return_documents = true; // 是否返回文档
+    // const res = await rerank(model, query, documents, return_documents)
+    // console.log(res)
 }
+
 (async () => {
     await main();
 })()
--- utils/siliconflow.js
+++ utils/siliconflow.js
@@ -1,19 +1,26 @@
 
 const axios = require('axios');
 
-async function chat(messages, model = 'Qwen/QwQ-32B-Preview') {
+/*
+* 对话
+* const messages = 
+[
+    { role: 'user', content: '你好！' }, // 提问历史
+    { role: 'assistant', content: '你好！需要什么帮助？' }, // 回答历史
+    { role: 'user', content: '告诉我关于AI的历史。' } // 当前提问
+]    // 内容
+*const model = 'Qwen/QwQ-32B-Preview'  // 模型
+*await chat(model,messages)
+*/
+async function chat(model, messages) {
     try {
         const response = await axios.post("https://api.siliconflow.cn/v1/chat/completions", {
             model,
-            messages: [
-                { role: 'user', content: '你好！' }, // 提问历史
-                { role: 'assistant', content: '你好！需要什么帮助？' }, // 回答历史
-                { role: 'user', content: '告诉我关于AI的历史。' } // 当前提问
-            ],
+            messages: messages,
             stream: false
         }, {
             headers: {
-                "Authorization": '',
+                "Authorization": "Bearer " + process.env.SILICONFLOW_API_KEY,
                 'Content-Type': 'application/json'
             }
         });
@@ -24,6 +31,37 @@ async function chat(messages, model = 'Qwen/QwQ-32B-Preview') {
     }
 }
 
+/*
+ * 文档重排
+ * const query = "人工智能的应用场景"; // 搜索关键词
+ * const documents = ["人工智能在医疗领域的应用","人工智能在教育领域的应用","人工智能在金融领域的应用","人工智能在农业领域的应用"]; // 文档列表
+ * const model = "netease-youdao/bce-reranker-base_v1"; // 模型
+ * const return_documents = true; // 是否返回文档
+ * await rerank(model, query, documents, return_documents)
+ * 
+ */
+async function rerank(model, query, documents, return_documents) {
+    try {
+        const data = {
+            model,
+            query,
+            documents,
+            return_documents,
+        };
+        const response = await axios.post("https://api.siliconflow.cn/v1/rerank", data, {
+            headers: {
+                "Authorization": "Bearer " + process.env.SILICONFLOW_API_KEY,
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log('文档重排成功:', response.data);
+        return response.data.results;
+    } catch (error) {
+        console.error('文档重排失败:', error.message);
+    }
+}
+
 module.exports = {
-    chat
+    chat,
+    rerank
 }
